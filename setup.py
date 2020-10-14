0d1357c41bab72dbb997563e08f0dc527096c96a
JKL404 <laxman.khatri@cs.christuniversity.in>
2020-10-14 09:24:21 +0530

    setup files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='Toggle',
+    url='https://github.com/JKL404/Toggle',
+    author='Laxman Khatri',
+    author_email='hackerlaxu@gmail.com',
+    # Needed to actually package something
+    packages=['Toggle'],
+    # Needed for dependencies
+    #install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='JKL404',
+    description='Toggle is a python package for string to toggle the given string',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
