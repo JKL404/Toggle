90c9d6ad65caf98ad9723e77bd9794bc60923c0c
Laxman Khatri <68042165+JKL404@users.noreply.github.com>
2020-10-14 11:10:07 +0530

    V 1.02
    Syntax:
    Toggle(string, choice)
    
    Example:
    Toggle("JokER beautiful",choice)
    
    Added Choice Option:
    __________________
    0: Default 
    output:  jOKer BEAUTIFUL
    
    1: Toggle with <lower><UPPER><lower><UPPER>....
    output: jOkEr BeAuTiFuL
    
    2: Toggle with <UPPER><lower><UPPER><lower>....
    output: JoKeR bEaUtIfUl
    
    3: Toggle with <lower><UPPER><UPPER>  <lower><UPPER><UPPER>....
    output: jOKER bEAUTIFUL
    
    4: Toggle with <UPPER><lower><lower>  <UPPER><lower><lower>....
    output: Joker Beautiful

--- Toggle/Toggle.py
+++ Toggle/Toggle.py
@@ -1,12 +1,12 @@
 # Python Package to Toggle Characters Case in a String by JKL404
  
 class Toggle:
-    def __new__(self,string="error: nO StRiNg FoUnD fOr ToGgLe", choice="a"):
+    def __new__(self,string="error: nO StRiNg FoUnD fOr ToGgLe", choice=0):
         #Initializing String and Choice
         self.choice=choice
         self.string = string
         self.string1 = ''
-        if self.choice=='a':
+        if self.choice==0:
             for i in self.string:
                 if(ord(i) >= 65 and ord(i) <= 90): 
                     self.string1 = self.string1 + chr((ord(i) + 32)) 
@@ -14,4 +14,61 @@ class Toggle:
                     self.string1 = self.string1 + chr((ord(i) - 32))
                 else:
                     self.string1 = self.string1 + i
+        elif self.choice==1 or self.choice==2:
+            if self.choice==1:
+                flag=0
+            else:
+                flag=1
+            for i in range(len(self.string)):
+                if self.string[i]==" ":
+                    if flag==0:
+                        flag=1
+                    else:
+                        flag=0
+                if i % 2 == flag:
+                    if(ord(self.string[i]) >= 65 and ord(self.string[i]) <= 90): 
+                        self.string1 = self.string1 + chr((ord(self.string[i]) + 32)) 
+                    else:
+                        self.string1 = self.string1 + self.string[i]
+                else:
+                    if(ord(self.string[i]) >= 97 and ord(self.string[i]) <= 122):
+                        self.string1 = self.string1 + chr((ord(self.string[i]) - 32))
+                    else:
+                        self.string1 = self.string1 + self.string[i]
+        elif self.choice==3 or self.choice==4:
+            if self.choice==3:
+                flag=0
+            else:
+                flag=1
+            for i in range(len(self.string)):
+                if flag==0:
+                    if i ==0:
+                        if(ord(self.string[i]) >= 65 and ord(self.string[i]) <= 90): 
+                            self.string1 = self.string1 + chr((ord(self.string[i]) + 32))
+                        else:
+                            self.string1 = self.string1 + self.string[i]
+                    elif self.string[i-1]==" ":
+                        if(ord(self.string[i+1]) >= 65 and ord(self.string[i+1]) <= 90): 
+                            self.string1 = self.string1 + chr((ord(self.string[i+1]) + 32))
+                        else:
+                            self.string1 = self.string1 + self.string[i]
+                    elif(ord(self.string[i]) >= 97 and ord(self.string[i]) <= 122):
+                        self.string1 = self.string1 + chr((ord(self.string[i]) - 32))
+                    else:
+                        self.string1 = self.string1 + self.string[i]
+                else:
+                    if i ==0:
+                        if(ord(self.string[i]) >= 97 and ord(self.string[i]) <= 122):
+                            self.string1 = self.string1 + chr((ord(self.string[i]) - 32))
+                        else:
+                            self.string1 = self.string1 + self.string[i]
+                    elif self.string[i-1]==" ":
+                        if(ord(self.string[i]) >= 97 and ord(self.string[i]) <= 122):
+                            self.string1 = self.string1 + chr((ord(self.string[i]) - 32))
+                        else:
+                            self.string1 = self.string1 + self.string[i]
+                    elif ord(self.string[i]) >= 65 and ord(self.string[i]) <= 90: 
+                            self.string1 = self.string1 + chr((ord(self.string[i]) + 32))
+                    else:
+                        self.string1 = self.string1 + self.string[i]
         return self.string1
